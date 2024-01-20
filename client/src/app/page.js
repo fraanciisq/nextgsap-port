290e0bb6fa37e32cc231632cec40b202a8fbe203
Francis <fraanciisq@gmail.com>
2024-01-21 00:21:08 +0800

    gsap-port-francis

---
+++ client/src/app/page.js
@@ -0,0 +1,40 @@
+'use client';
+import styles from "./page.module.css";
+import Image from "next/image";
+import gsap from 'gsap';
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect( () => {
+    requestAnimationFrame(animation);
+  }, [])
+
+  const animation = () => {
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
+    xPercent += 0.1 * direction;
+    requestAnimationFrame(animation);
+  }   
+  
+  return (
+    <main className={styles.main}>
+      <Image
+      fill ={true}
+      src="/images/background.jpg"
+      alt="image"
+      />
+      <div className={styles.sliderContainer}>
+        <div className={styles.slider}>
+          <p ref={firstText}> Software Developer - </p>
+          <p ref={secondText}> Software Developer - </p>
+        </div>
+      </div>
+    </main>
+  );
+}
