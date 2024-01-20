9e81e62deb2f5880a8b278e85e510562a2a94d41
Francis <fraanciisq@gmail.com>
2024-01-21 01:12:27 +0800

    Commit GSAP

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,25 +3,58 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import gsap from 'gsap';
 import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Home() {
 
   const firstText = useRef(null);
   const secondText = useRef(null);
+  const slider = useRef(null);
   let xPercent = 0;
   let direction = -1;
 
   useEffect( () => {
-    requestAnimationFrame(animation);
+    gsap.registerPlugin(ScrollTrigger);
+    requestAnimationFrame(animate);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.5,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: e => direction = e.direction * -1
+      },
+      x: "-500px",
+
+
+    })
+
   }, [])
 
-  const animation = () => {
+  const animate = () => {
+
+    if(xPercent < -100){
+
+      xPercent = 0;
+
+    }
+
+    else if(xPercent > 0){
+
+      xPercent = -100;
+
+    }
+
     gsap.set(firstText.current, {xPercent: xPercent})
+
     gsap.set(secondText.current, {xPercent: xPercent})
+
+    requestAnimationFrame(animate);
+
     xPercent += 0.1 * direction;
-    requestAnimationFrame(animation);
-  }   
-  
+
+  }
   return (
     <main className={styles.main}>
       <Image
@@ -30,9 +63,9 @@ export default function Home() {
       alt="image"
       />
       <div className={styles.sliderContainer}>
-        <div className={styles.slider}>
-          <p ref={firstText}> Software and Creative Dev - </p>
-          <p ref={secondText}> Software and Creative Dev - </p>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}> Software Developer - </p>
+          <p ref={secondText}> Software Developer - </p>
         </div>
       </div>
     </main>
